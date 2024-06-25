9fcb594edc3ab5813bd0497a18ff8bd4fc12a844
Alexander_Kukharev <akukharev@setronica.com>
2024-06-25 18:56:42 +0200

    Updated repository Delete ExpiryTime var and everything linked to that

--- src/main/java/com/kukharev/pastbin/model/TextBlock.java
+++ src/main/java/com/kukharev/pastbin/model/TextBlock.java
@@ -1,11 +1,17 @@
 package com.kukharev.pastbin.model;
 
+import com.kukharev.pastbin.repository.TextBlockRepository;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.time.LocalDateTime;
+import java.util.Base64;
 
 @Entity
 public class TextBlock {
@@ -18,24 +24,29 @@ public class TextBlock {
 
     private String text;
 
-    private LocalDateTime expiryTime;
+    private LocalDateTime createdAt;
 
-    public void setText(String text) {
-    }
-
-    public void setExpiryTime(LocalDateTime plusSeconds) {
+    public String getHash() {
+        return hash;
     }
 
     public void setHash(String hash) {
-    }
-
-    public String getHash() {
-        return null;
+        this.hash = hash;
     }
 
     public String getText() {
-        return null;
+        return text;
     }
 
-    // Getters and setters
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }

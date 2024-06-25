ce194c22861da33b51641c4771cef9173ece628e
Alexander_Kukharev <akukharev@setronica.com>
2024-06-25 18:55:49 +0200

    Updated service Delete ExpiryTime and all linked to that

--- src/main/java/com/kukharev/pastbin/controller/TextBlockRequest.java
+++ src/main/java/com/kukharev/pastbin/controller/TextBlockRequest.java
@@ -2,21 +2,13 @@ package com.kukharev.pastbin.controller;
 
 public class TextBlockRequest {
     private String text;
-    private long expiryTime;
 
     public String getText() {
-        return null;
+        return text;
     }
 
-    public long getExpiryTime() {
-        return 0;
+    public void setText(String text) {
+        this.text = text;
     }
-
-    public void setExpiryTime(int i) {
-    }
-
-    public void setText(String test_text) {
-    }
-
-    // Getters and setters
 }
+
--- src/main/java/com/kukharev/pastbin/service/TextBlockService.java
+++ src/main/java/com/kukharev/pastbin/service/TextBlockService.java
@@ -3,6 +3,7 @@ package com.kukharev.pastbin.service;
 import com.kukharev.pastbin.exception.ResourceNotFoundException;
 import com.kukharev.pastbin.model.TextBlock;
 import com.kukharev.pastbin.repository.TextBlockRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,11 +18,12 @@ public class TextBlockService {
     @Autowired
     private HashGeneratorService hashGeneratorService;
 
-    public String createTextBlock(String text, long expiryTime) {
+    @Transactional
+    public String createTextBlock(String text) {
         TextBlock textBlock = new TextBlock();
         textBlock.setText(text);
-        textBlock.setExpiryTime(LocalDateTime.now().plusSeconds(expiryTime));
-        String hash = hashGeneratorService.generateHash(text + System.currentTimeMillis());
+        textBlock.setCreatedAt(LocalDateTime.now());
+        String hash = hashGeneratorService.generateHash(text);
         textBlock.setHash(hash);
         textBlockRepository.save(textBlock);
         return hash;
@@ -29,6 +31,15 @@ public class TextBlockService {
 
     public TextBlock getTextBlock(String hash) {
         return textBlockRepository.findByHash(hash)
+                .orElseThrow(() -> new ResourceNotFoundException
+                        ("TextBlock not found with hash " + hash));
+    }
+
+    @Transactional
+    public void deleteTextBlock(String hash) {
+        TextBlock textBlock = textBlockRepository.findByHash(hash)
                 .orElseThrow(() -> new ResourceNotFoundException("TextBlock not found with hash " + hash));
+
+        textBlockRepository.delete(textBlock);
     }
 }
